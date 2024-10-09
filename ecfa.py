9cf3c3e30620a4bed3e41182323d00f63b0934df
michal.bozek <michal.bozek@bisimulations.com>
2024-10-10 01:26:39 +0200

    returnig keys

--- ecfa.py
+++ ecfa.py
@@ -11,16 +11,18 @@ class OxfrodLearners5000Spider(scrapy.Spider):
     return 'https://www.oxfordlearnersdictionaries.com' + href
 
   def start_requests(self):
-    response = requests.get(
-      'https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000',
-      headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
-      )
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0'}
+    response = requests.get('https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000', headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     wordlist = soup.find('ul', class_='top-g')
     words = wordlist.find_all('li')
-    urls = [self._word_to_url(word) for word in words]
+    # urls = [self._word_to_url(word) for word in words]
+    urls = ['https://www.oxfordlearnersdictionaries.com/definition/english/abolish', 'https://www.oxfordlearnersdictionaries.com/definition/english/a_1']
     for url in urls:
-      yield scrapy.Request(url)
+      yield scrapy.Request(url, headers=headers)
 
   def parse(self, response):
-      pass
+    entry = response.css('div.entry')[0]
+    name = entry.css('div.top-container h1::text').get()
+    yield { 'entry': name }
+
